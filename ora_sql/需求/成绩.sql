e9feeca1e5459efa636951235d8527411913306d
CHNnoodle <625887616@qq.com>
2017-01-05 16:51:56 +0800

    1

--- ora_sql/需求/成绩.sql
+++ ora_sql/需求/成绩.sql
@@ -8,8 +8,10 @@ insert into xijiaweb.undergraduate_course_results
          t2.kcmc course_name,
          t1.xf   credit_points,
          t1.zpcj result
-    from xj_gl.t_d_bzks_kscj t1,xj_gl.t_d_bzks_kc t2
+    from xj_gl.t_d_bzks_kscj t1,xj_gl.t_d_bzks_kc t2,xj_gl.t_d_bzks t3
    where t1.kcdm=t2.kcdm(+)
+   and t1.xh=t3.xh
    and t1.partid = '03'
-   and t2.partid='03';
+   and t2.partid='03'
+   and t3.partid='03';
    commit;
