4de1295a13922513ffe6c22a04c8016a59553637
wanggang <625887616@qq.com>
2017-02-22 09:53:16 +0800

    1

--- ora_sql/需求/图书信息.sql
+++ ora_sql/需求/图书信息.sql
@@ -7,3 +7,15 @@ insert into xijiaweb.library_books
          dyzz  author
     from stage.t_ts;
     commit;
+
+truncate table xijiaweb.library_books;
+insert into xijiaweb.library_books
+  (book_code, library_code, book_name, year, author,call_no)
+  select tstxm book_code,
+         gcdm  library_code,
+         tm    book_name,
+         cbrq  year,
+         dyzz  author,
+         t2.category_code call_no
+    from stage.t_ts t1,xijiaweb.library_book_book_categories t2
+    where t1.tstxm=t2.book_code(+)
